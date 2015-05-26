a4cca819ab305710ac6c6ed13e0fd675c35fe196
liyin <liy@soulgame.com>
2015-05-26 18:36:18 +0800

    【学习Xv6】加载并运行内核
    【学习Xv6】加载并运行内核

--- bootmain.c
+++ bootmain.c
@@ -10,7 +10,7 @@
 #include "x86.h"
 #include "memlayout.h"
 
-#define SECTSIZE  512
+#define SECTSIZE  512  // 硬盘扇区大小 512 字节
 
 void readseg(uchar*, uint, uint);
 
@@ -24,25 +24,30 @@ bootmain(void)
 
   elf = (struct elfhdr*)0x10000;  // scratch space
 
-  // Read 1st page off disk
+  // 从内核所在硬盘位置读取一内存页 4kb 数据
   readseg((uchar*)elf, 4096, 0);
 
-  // Is this an ELF executable?
+  // 判断是否为 ELF 文件格式
   if(elf->magic != ELF_MAGIC)
     return;  // let bootasm.S handle error
 
-  // Load each program segment (ignores ph flags).
+  // 加载 ELF 文件中的程序段 (ignores ph flags).
+  // 找到内核 ELF 文件的程序头表
   ph = (struct proghdr*)((uchar*)elf + elf->phoff);
+  // 内核 ELF 文件程序头表的结束位置
   eph = ph + elf->phnum;
+  // 开始将内核 ELF 文件程序头表载入内存
   for(; ph < eph; ph++){
     pa = (uchar*)ph->paddr;
     readseg(pa, ph->filesz, ph->off);
+    // 如果内存大小大于文件大小，用 0 补齐内存空位
     if(ph->memsz > ph->filesz)
       stosb(pa + ph->filesz, 0, ph->memsz - ph->filesz);
   }
 
   // Call the entry point from the ELF header.
   // Does not return!
+  // 从内核 ELF 文件入口点开始执行内核
   entry = (void(*)(void))(elf->entry);
   entry();
 }
@@ -51,22 +56,40 @@ void
 waitdisk(void)
 {
   // Wait for disk ready.
+  // xxxxxxxx & 11000000 != 01000000
+  // 0x1F7 状态位
+  //     bit 7 = 1  控制器忙
+  //     bit 6 = 1  驱动器就绪
+  //     bit 5 = 1  设备错误
+  //     bit 4        N/A
+  //     bit 3 = 1  扇区缓冲区错误
+  //     bit 2 = 1  磁盘已被读校验
+  //     bit 1        N/A
+  //     bit 0 = 1  上一次命令执行失败
   while((inb(0x1F7) & 0xC0) != 0x40)
     ;
 }
 
 // Read a single sector at offset into dst.
+// 这里使用的是 LBA 磁盘寻址模式
+// LBA是非常单纯的一种寻址模式﹔从0开始编号来定位区块，
+// 第一区块LBA=0，第二区块LBA=1，依此类推
 void
-readsect(void *dst, uint offset)
+readsect(void *dst, uint offset)      // 0x10000, 1
 {
   // Issue command.
   waitdisk();
-  outb(0x1F2, 1);   // count = 1
-  outb(0x1F3, offset);
-  outb(0x1F4, offset >> 8);
-  outb(0x1F5, offset >> 16);
-  outb(0x1F6, (offset >> 24) | 0xE0);
-  outb(0x1F7, 0x20);  // cmd 0x20 - read sectors
+  outb(0x1F2, 1);                     // 要读取的扇区数量 count = 1
+  outb(0x1F3, offset);                // 扇区 LBA 地址的 0-7 位
+  outb(0x1F4, offset >> 8);           // 扇区 LBA 地址的 8-15 位
+  outb(0x1F5, offset >> 16);          // 扇区 LBA 地址的 16-23 位
+  outb(0x1F6, (offset >> 24) | 0xE0); // offset | 11100000 保证高三位恒为 1
+                                      //         第7位     恒为1
+                                      //         第6位     LBA模式的开关，置1为LBA模式
+                                      //         第5位     恒为1
+                                      //         第4位     为0代表主硬盘、为1代表从硬盘
+                                      //         第3~0位   扇区 LBA 地址的 24-27 位
+  outb(0x1F7, 0x20);                  // 20h为读，30h为写
 
   // Read data.
   waitdisk();
@@ -76,21 +99,23 @@ readsect(void *dst, uint offset)
 // Read 'count' bytes at 'offset' from kernel into physical address 'pa'.
 // Might copy more than asked.
 void
-readseg(uchar* pa, uint count, uint offset)
+readseg(uchar* pa, uint count, uint offset)  // 0x10000, 4096(0x1000), 0
 {
   uchar* epa;
 
-  epa = pa + count;
+  epa = pa + count;  // 0x11000
 
-  // Round down to sector boundary.
+  // 根据扇区大小 512 字节做对齐
   pa -= offset % SECTSIZE;
 
-  // Translate from bytes to sectors; kernel starts at sector 1.
+  // bootblock 引导区在第一扇区（下标为 0），内核在第二个扇区（下标为 1）
+  // 这里做 +1 操作是统一略过引导区
   offset = (offset / SECTSIZE) + 1;
 
   // If this is too slow, we could read lots of sectors at a time.
   // We'd write more to memory than asked, but it doesn't matter --
   // we load in increasing order.
+  // 一次读取一个扇区 512 字节的数据
   for(; pa < epa; pa += SECTSIZE, offset++)
     readsect(pa, offset);
 }
--- elf.h
+++ elf.h
@@ -4,33 +4,70 @@
 
 // File header
 struct elfhdr {
-  uint magic;  // must equal ELF_MAGIC
-  uchar elf[12];
-  ushort type;
-  ushort machine;
-  uint version;
-  uint entry;
-  uint phoff;
-  uint shoff;
-  uint flags;
-  ushort ehsize;
-  ushort phentsize;
-  ushort phnum;
-  ushort shentsize;
-  ushort shnum;
-  ushort shstrndx;
+  uint magic;       // 4 字节，始终为 0x7felf 表名该文件是个 ELF 格式文件
+
+  uchar elf[12];    // 12 字节，每字节对应意义如下：
+                    //     0 : 1 = 32 位程序；2 = 64 位程序
+                    //     1 : 数据编码方式，0 = 无效；1 = 小端模式；2 = 大端模式
+                    //     2 : 只是版本，固定为 0x1
+                    //     3 : 目标操作系统架构
+                    //     4 : 目标操作系统版本
+                    //     5 ~ 11 : 固定为 0
+                    
+  ushort type;      // 2 字节，表明该文件类型，意义如下：
+                    //     0x0 : 未知目标文件格式
+                    //     0x1 : 可重定位文件
+                    //     0x2 : 可执行文件
+                    //     0x3 : 共享目标文件
+                    //     0x4 : 转储文件
+                    //     0xff00 : 特定处理器文件
+                    //     0xffff : 特定处理器文件
+
+  ushort machine;   // 2 字节，表明运行该程序需要的计算机体系架构，这里我们只需要知道 0x0 为未指定；0x3 为 x86 架构
+
+  uint version;     // 4 字节，表示该文件的版本号
+
+  uint entry;       // 4 字节，该文件的入口地址，没有入口（非可执行文件）则为 0
+
+  uint phoff;       // 4 字节，表示该文件的“程序头部表”相对于文件的位置，单位是字节
+
+  uint shoff;       // 4 字节，表示该文件的“节区头部表”相对于文件的位置，单位是字节
+
+  uint flags;       // 4 字节，特定处理器标志
+
+  ushort ehsize;    // 2 字节，ELF文件头部的大小，单位是字节
+
+  ushort phentsize; // 2 字节，表示程序头部表中一个入口的大小，单位是字节
+
+  ushort phnum;     // 2 字节，表示程序头部表的入口个数，phnum * phentsize = 程序头部表大小（单位是字节）
+
+  ushort shentsize; // 2 字节，节区头部表入口大小，单位是字节
+
+  ushort shnum;     // 2 字节，节区头部表入口个数，shnum * shentsize = 节区头部表大小（单位是字节）
+
+  ushort shstrndx;  // 2 字节，表示字符表相关入口的节区头部表索引
 };
 
 // Program section header
 struct proghdr {
-  uint type;
-  uint off;
-  uint vaddr;
-  uint paddr;
-  uint filesz;
-  uint memsz;
-  uint flags;
-  uint align;
+  uint type;        // 4 字节， 段类型
+                    //         1 PT_LOAD : 可载入的段
+                    //         2 PT_DYNAMIC : 动态链接信息
+                    //         3 PT_INTERP : 指定要作为解释程序调用的以空字符结尾的路径名的位置和大小
+                    //         4 PT_NOTE : 指定辅助信息的位置和大小
+                    //         5 PT_SHLIB : 保留类型，但具有未指定的语义
+                    //         6 PT_PHDR : 指定程序头表在文件及程序内存映像中的位置和大小
+                    //         7 PT_TLS : 指定线程局部存储模板
+  uint off;         // 4 字节， 段的第一个字节在文件中的偏移
+  uint vaddr;       // 4 字节， 段的第一个字节在内存中的虚拟地址
+  uint paddr;       // 4 字节， 段的第一个字节在内存中的物理地址(适用于物理内存定位型的系统)
+  uint filesz;      // 4 字节， 段在文件中的长度
+  uint memsz;       // 4 字节， 段在内存中的长度
+  uint flags;       // 4 字节， 段标志
+                    //         1 : 可执行
+                    //         2 : 可写入
+                    //         4 : 可读取
+  uint align;       // 4 字节， 段在文件及内存中如何对齐
 };
 
 // Values for Proghdr type
